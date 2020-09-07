e1f20cbcbb65875f866554174b7b53d9faed7be0
abhi-ezc <abhinavchendayad@gmail.com>
2020-09-07 18:39:54 +0530

    Program 3 Question Changed

--- Program3.java
+++ Program3.java
@@ -6,19 +6,15 @@ public class Program3
         System.out.println("Input>");
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
+        int count=1;
         for(int i=1;i<=n;i++)
         {
-          if(i%2==1)
-          {
-            if(i==1)
+            if(i%2==0 && i==n)
             {
-                System.out.print(i);
+                break;
             }
-            else
-            {
-              System.out.print(","+i); 
-            }
-          }
+            System.out.print(count);
+            count+=2;
         }
 	}
 }
